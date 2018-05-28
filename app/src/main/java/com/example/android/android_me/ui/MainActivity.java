f9c264e696bb8d2da1c80b94cf4171bd700be83c
Amit Grover <amitg90@gmail.com>
2018-05-27 21:50:53 -0700

    finished TFragments.05-Exercise-InterfaceCommunication

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -16,21 +16,27 @@
 
 package com.example.android.android_me.ui;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
 // TODO (4) Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    public void onImageSelected(int position) {
+        Toast.makeText(this, "Clicked Position:" + position, Toast.LENGTH_LONG).show();
     }
 
     // TODO (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
